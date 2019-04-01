2e923d7e719784f171d8e10b8f70feb2a8044de6
Gary Ditsch <garyditsch@gmail.com>
2019-04-01 11:15:59 -0400

    practicing js with class

--- main.js
+++ main.js
@@ -1,7 +1,35 @@
-var image = document.getElementById('the-image');
-console.log('added event lister')
-console.log(image)
+// var image = document.getElementById('the-image');
+// console.log('added event lister')
+// console.log(image)
 
-image.addEventListener('click', event => {
-    image.src = "./images/pexels-photo-2.jpeg"
-});
+// image.addEventListener('click', event => {
+//     image.src = "./images/pexels-photo-2.jpeg"
+// });
+
+// const getGPA = function(){
+//     console.log('hi')
+// }
+// getGPA()
+
+// for(i = 5; i <= 10; i++){
+//     console.log(i)
+// }
+
+var happy = 11;
+console.log('happy is:', happy)
+
+// for(happy; happy > 0; happy--){
+//     console.log(happy)
+// }
+
+// while(happy > 0){
+//     console.log(happy--)
+// }
+
+if( happy < 9){
+    console.log('This statement is true also')
+} else if ( happy > 10){
+    console.log('Happy is greater than 10')
+} else {
+    console.log('The first one was false, this is true')
+}
